e72441f04bc2864f0f08caecda63be45e092099d
David Lin <davidzylin@outlook.com>
2021-04-21 15:56:40 -0700

    Update readability.c

--- pset2/readability.c
+++ pset2/readability.c
@@ -1,29 +1,91 @@
   
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int count_letters(string text);
+int count_sentences(string text);
+int count_words(string text);
+void readability_level(string text);
 
 int main(void)
 {
-    int height;
-    do
+    string str = get_string("Text: ");
+    readability_level(str);
+}
+
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        height = get_int("Height: ");
-    }
-    while (height < 1 || height > 8);
-    
-    for (int i = 1; i <= height; i++)
-    {
-        for (int j = 0; j < height + i + 2; j++)
+        if (isalpha(text[i]))
         {
-            if (j != height && j != height + 1 && j >= height - i)
-            {
-                printf("#");
-            }
-            else
-            {
-                printf(" ");
-            }
+            count++;
         }
-        printf("\n");
+    }
+    
+    return count;
+}
+
+int count_words(string text)
+{
+    int count = 0;
+    
+    if (text[0] != '\0')
+    {
+        count++;
+    }
+    
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isspace(text[i]))
+        {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+void readability_level(string text)
+{
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+    int letters = count_letters(text);
+    
+    float L = 100.0 * letters / words;
+    float S = 100.0 * sentences / words;
+    
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+    
+    int level = (int)(index < 0 ? (index - 0.5) : (index + 0.5));
+    
+    if (level < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (level >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", level);
     }
 }
